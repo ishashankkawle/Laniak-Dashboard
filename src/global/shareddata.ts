3d44a626be1612ad4fb42ebbfcf96aa3aa6893be
ShashankKawle <shashankkawle67@gmail.com>
2018-09-02 20:37:01 +0530

    1) set repo to 'DOCS'

--- src/global/shareddata.ts
+++ src/global/shareddata.ts
@@ -2,5 +2,5 @@ export class SharedDataAssets
 {
     static BASE_URI : string = "https://api.github.com/repos"
     static USER_NAME : string = "Shashankkawle"
-    static REPOSITORY_NAME : string = "JSBasics"
+    static REPOSITORY_NAME : string = "DOCS"
 }
